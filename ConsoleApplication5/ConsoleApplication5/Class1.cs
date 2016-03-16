d85a88ca6acf9b744f56a6d366acdb1967f10752
salman AL-FARSI <salmanmasood24@yahoo.com>
2016-03-15 19:02:52 -0700

    error resolved in method

--- ConsoleApplication5/ConsoleApplication5/Class1.cs
+++ ConsoleApplication5/ConsoleApplication5/Class1.cs
@@ -8,5 +8,39 @@ namespace ConsoleApplication5
 {
     class Class1
     {
+
+      
+        public void processmethod(int[] z, int logicalsize, int iz, int zi, int quantum, int phyicalsize,int zflag)
+        {
+
+            if (iz + zi <= logicalsize)
+            {
+                for (int i = iz; i < zi; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("process z is running...with quantum of {0} second and process # {1}", (i + 1), z[i]);
+                }
+                zi = zi + quantum;
+                iz = iz + quantum;
+                phyicalsize = phyicalsize + quantum;
+            }
+            else if (zflag == 0)
+            {
+                int help = 0;
+                for (int i = iz; i < logicalsize; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("process z is running...with quantum of {0} second and process # {1}", (i + 1), z[i]);
+                    help++;
+                }
+                phyicalsize = phyicalsize + help;
+                zflag = 1;
+            }
+         
+        }
+
+
+
+
     }
 }
